19d724eeeda0dcddab9672d331118ec5594811e7
RiccardoParrino <riccardoparrino1@gmail.com>
2025-07-01 12:46:27 +0200

    use a component inside another one component

--- angular-first-app/src/user-list/user-list.component.ts
+++ angular-first-app/src/user-list/user-list.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NgFor } from '@angular/common';
+import { UserItemComponent } from '../user-item/user-item.component';
 
 @Component({
   selector: 'app-user-list',
-  imports: [NgFor],
+  imports: [NgFor, UserItemComponent],
   templateUrl: './user-list.component.html',
   styleUrl: './user-list.component.css'
 })
