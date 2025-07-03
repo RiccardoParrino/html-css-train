2847de8fcf55739ac4e37c05e24734e3e2c1dc2f
RiccardoParrino <riccardoparrino1@gmail.com>
2025-07-03 15:27:58 +0200

    new component variable lenght

--- angular-reddit/src/app/app.component.ts
+++ angular-reddit/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ArticleComponent } from './article/article.component';
+import { Article } from './article/article.model';
 
 @Component({
   selector: 'app-root',
@@ -10,9 +11,17 @@ import { ArticleComponent } from './article/article.component';
 })
 export class AppComponent {
   title = 'angular-reddit';
+  articles: Article[];
+
+  constructor () {
+    this.articles = [];
+  }
 
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
     console.log(`Adding article title: ${title.value} and link: ${link.value}`)
+    this.articles.push(new Article(title.value, link.value, 0));
+    title.value = '';
+    link.value = '';
     return false;
   }
 }
--- angular-reddit/src/app/article/article.component.ts
+++ angular-reddit/src/app/article/article.component.ts
@@ -1,23 +1,20 @@
-import { Component, HostBinding, OnInit } from '@angular/core';
+import { Component, HostBinding, OnInit, Input } from '@angular/core';
 import { Article } from './article.model';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-article',
-  imports: [],
+  imports: [NgFor],
   templateUrl: './article.component.html',
   styleUrl: './article.component.css'
 })
 export class ArticleComponent implements OnInit {
 
   @HostBinding('attr.class') cssClass = 'row';
-  article: Article;
+  @Input()
+  article!: Article;
 
-  constructor() {
-    this.article = new Article(
-      'Angular',
-      'http://angular.io',
-      10);
-  }
+  constructor() {}
 
   voteUp(): boolean {
     this.article.voteUp();
