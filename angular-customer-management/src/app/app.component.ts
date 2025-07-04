df8210f203a3b13c1be4b44046fea3e6412820ce
RiccardoParrino <riccardoparrino1@gmail.com>
2025-07-04 11:16:55 +0200

    sidenav angular material

--- angular-customer-management/src/app/app.component.ts
+++ angular-customer-management/src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSidenavModule, MatSidenavContent } from '@angular/material/sidenav'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatSidenavModule, MatSidenavContent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  
   title = 'angular-customer-management';
+
 }
