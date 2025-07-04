b37f93ff931aad35530e62517189939bc72e2634
RiccardoParrino <riccardoparrino1@gmail.com>
2025-07-04 11:55:45 +0200

    sidenav under toolbar in app.component

--- angular-customer-management/src/app/app.component.ts
+++ angular-customer-management/src/app/app.component.ts
@@ -3,10 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { MatSidenavModule, MatSidenavContent } from '@angular/material/sidenav'
 import { MatToolbar } from '@angular/material/toolbar'
 import { MatIcon } from '@angular/material/icon'
+import { MatIconButton } from '@angular/material/button'
+import { SidenavComponent } from './sidenav/sidenav.component';
 
 @Component({
   selector: 'app-root',
-  imports: [MatIcon, MatToolbar, RouterOutlet, MatSidenavModule, MatSidenavContent],
+  imports: [SidenavComponent, MatIconButton, MatIcon, MatToolbar, RouterOutlet, MatSidenavModule, MatSidenavContent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- angular-customer-management/src/app/sidenav/sidenav.component.ts
+++ angular-customer-management/src/app/sidenav/sidenav.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import {MatSidenavContainer, MatSidenavContent, MatSidenavModule} from '@angular/material/sidenav'
 
 @Component({
   selector: 'app-sidenav',
-  imports: [],
+  imports: [MatSidenavModule],
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.css'
 })
