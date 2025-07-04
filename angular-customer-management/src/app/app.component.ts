aa05c217c8cf606f85ddfec4487ae6e61db1fa8d
RiccardoParrino <riccardoparrino1@gmail.com>
2025-07-04 11:39:03 +0200

    adding toolbar to angular app component

--- angular-customer-management/src/app/app.component.ts
+++ angular-customer-management/src/app/app.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatSidenavModule, MatSidenavContent } from '@angular/material/sidenav'
+import { MatToolbar } from '@angular/material/toolbar'
+import { MatIcon } from '@angular/material/icon'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatSidenavModule, MatSidenavContent],
+  imports: [MatIcon, MatToolbar, RouterOutlet, MatSidenavModule, MatSidenavContent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  
   title = 'angular-customer-management';
-
 }
