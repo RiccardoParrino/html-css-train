e4c20ea5416784c623e3ab63c8a06f2aef4556de
RiccardoParrino <riccardoparrino1@gmail.com>
2025-07-05 13:28:47 +0200

    there is something goes wrong

--- angular-inventory-management/src/app/product-list/product-list.component.ts
+++ angular-inventory-management/src/app/product-list/product-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ProductComponent } from '../product/product.component';
 
 @Component({
@@ -8,7 +8,36 @@ import { ProductComponent } from '../product/product.component';
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent implements OnInit {
+  @Input() products: ProductComponent[];
+  p1: ProductComponent;
+  p2: ProductComponent;
+  p3: ProductComponent;
+  
   constructor() {
+    this.products = [];
+
+    this.p1 = new ProductComponent();
+    this.p2 = new ProductComponent();
+    this.p3 = new ProductComponent();
+
+    this.p1.department = "p1Department";
+    this.p1.image = "p1Image";
+    this.p1.price = "p1Price";
+    this.p1.product_name = "p1ProductName";
+
+    this.p2.department = "p2Department";
+    this.p2.image = "p2Image";
+    this.p2.price = "p2Price";
+    this.p2.product_name = "p2ProductName";
+
+    this.p3.department = "p3Department";
+    this.p3.image = "p3Image";
+    this.p3.price = "p3Price";
+    this.p3.product_name = "p3ProductName";
+
+    this.products.push(this.p1);
+    this.products.push(this.p2);
+    this.products.push(this.p3);
   }
 
   ngOnInit(): void {
