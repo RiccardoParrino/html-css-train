5d1fe409deea33550f1f7b475d3ed8a755ad5a34
RiccardoParrino <riccardoparrino1@gmail.com>
2025-07-05 17:46:50 +0200

    adding product-list component

--- angular-inventory-management-2/src/app/app.component.ts
+++ angular-inventory-management-2/src/app/app.component.ts
@@ -11,15 +11,35 @@ import { Product } from './product/product.model';
 export class AppComponent {
   title = 'angular-inventory-management-2';
 
-  product: Product;
+  products: Product[];
 
   constructor() {
-    this.product = new Product(
-      "NICEHAT",
-      "A Nice Black Hat",
-      "/resources/image/products/black-hat.jpg",
-      ["Men", "Accessories",  "Hats"],
-      29.99
-    );
+    this.products = [ 
+      new Product(
+        "NICEHAT",
+        "A Nice Black Hat",
+        "/resources/image/products/black-hat.jpg",
+        ["Men", "Accessories",  "Hats"],
+        29.99
+      ),
+      new Product (
+        "NEATOJACKET",
+        "Blue Jacket",
+        "assets/images/products/blue-jacket.jpg",
+        ["Women",  "Apparel", "Jackets & Vests"],
+        238.99
+      ),
+      new Product (
+        "MYSHOES",
+        "Black Running Shoes",
+        "/assets/images/products/black-soes.jpg",
+        ["Men",  "Shoes", "Running Shoes"],
+        109.99
+      )
+   ];
+  }
+
+  productWasSelected(product: Product): void {
+    console.log('Product clicked: ', product);
   }
 }
