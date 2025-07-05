ae7a0d76b9a14660e3c75056dc6fef9795dea4ba
RiccardoParrino <riccardoparrino1@gmail.com>
2025-07-05 10:50:40 +0200

    product element definintion

--- angular-inventory-management/src/app/product/product.component.ts
+++ angular-inventory-management/src/app/product/product.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { DepartmentComponent } from './department/department.component';
+import { ImageComponent } from './image/image.component';
+import { PriceComponent } from './price/price.component';
 
 @Component({
   selector: 'app-product',
@@ -7,5 +10,14 @@ import { Component } from '@angular/core';
   styleUrl: './product.component.css'
 })
 export class ProductComponent {
+  department: DepartmentComponent;
+  image: ImageComponent;
+  price: PriceComponent;
+
+  constructor (department:DepartmentComponent, image:ImageComponent, price:PriceComponent) {
+    this.department = department;
+    this.image = image;
+    this.price = price;
+  }
 
 }
