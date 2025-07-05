a14206fcf216756afa165c6dc87b0c3eaa763c88
RiccardoParrino <riccardoparrino1@gmail.com>
2025-07-05 09:21:34 +0200

    adding app.component imports

--- angular-inventory-management/src/app/app.component.ts
+++ angular-inventory-management/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductListComponent } from './product-list/product-list.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProductListComponent, NavigationComponent, BreadcrumbComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
