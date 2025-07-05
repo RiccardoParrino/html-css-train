2e8f5249cd874fabadcadad9520a2e2918ff6b9e
RiccardoParrino <riccardoparrino1@gmail.com>
2025-07-05 17:36:02 +0200

    update index and app.component.html

--- angular-inventory-management-2/src/app/app.component.ts
+++ angular-inventory-management-2/src/app/app.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Product } from './product/product.model';
 
 @Component({
-  selector: 'app-root',
+  selector: 'inventory-app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular-inventory-management-2';
+
+  product: Product;
+
+  constructor() {
+    this.product = new Product(
+      "NICEHAT",
+      "A Nice Black Hat",
+      "/resources/image/products/black-hat.jpg",
+      ["Men", "Accessories",  "Hats"],
+      29.99
+    );
+  }
 }
