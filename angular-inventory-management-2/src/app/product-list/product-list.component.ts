2e01baf9f29000111af11603919d6ad1d892b819
RiccardoParrino <riccardoparrino1@gmail.com>
2025-07-07 15:36:59 +0200

    fixing passing input in Angular

--- angular-inventory-management-2/src/app/product-list/product-list.component.ts
+++ angular-inventory-management-2/src/app/product-list/product-list.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Product } from '../product/product.model';
 import { NgFor } from '@angular/common';
+import { ProductComponent } from '../product/product.component';
 
 @Component({
   selector: 'app-product-list',
-  imports: [NgFor],
+  imports: [NgFor, ProductComponent],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
