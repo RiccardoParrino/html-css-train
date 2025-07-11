7ef7bcc70825f45f6652f9f943c15f9095abc6fb
RiccardoParrino <riccardoparrino1@gmail.com>
2025-07-11 17:00:35 +0200

    template for search component

--- angular-spotify-app/src/app/search/search.component.ts
+++ angular-spotify-app/src/app/search/search.component.ts
@@ -9,5 +9,34 @@ import { SpotifyService } from '../service/spotify/spotify.service';
   styleUrl: './search.component.css'
 })
 export class SearchComponent {
+  query: string = '';
+  results: Object = new Object();
+
+  constructor(
+    private spotify: SpotifyService,
+    private router: Route,
+    private route: ActivatedRoute) {
+      this.route
+        .queryParams
+        .subscribe(params => {this.query = params['query'] || ''; })
+  }
+
+  search(): void {
+    console.log('this.query', this.query);
+
+    if (!this.query){
+      return;
+    }
+
+    this.spotify
+      .searchTrack(this.query)
+      .subscribe( (res:any) => this.renderResults(res) );
+  }
+
+  renderResults(res: any): void {
+    if (res && res.tracks && res.tracks.items) {
+      this.results = res.tracks.items;
+    }
+  }
 
 }
