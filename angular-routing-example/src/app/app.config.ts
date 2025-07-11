48853ec666699f99ec073ef7f94566b0fb2aa40d
RiccardoParrino <riccardoparrino1@gmail.com>
2025-07-11 09:51:40 +0200

    using the hashLocationStrategy

--- angular-routing-example/src/app/app.config.ts
+++ angular-routing-example/src/app/app.config.ts
@@ -1,11 +1,14 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter, RouterModule } from '@angular/router';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
-    provideRouter(routes)
+    provideRouter(routes),
+    importProvidersFrom(RouterModule),
+    {provide: LocationStrategy, useClass:HashLocationStrategy}
   ]
 };
