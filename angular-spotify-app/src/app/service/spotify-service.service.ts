195be6144cbbcf21f781cbc417aa8cb6ab745e77
RiccardoParrino <riccardoparrino1@gmail.com>
2025-07-11 16:31:52 +0200

    new SearchComponent

---
+++ angular-spotify-app/src/app/service/spotify-service.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SpotifyServiceService {
+
+  constructor(public http: HttpClient) { }
+
+  searchTrack(query: string) {
+    let params: string = [
+      `q=${query}`,
+      `type=track`
+    ].join("&");
+    let queryURL: string = `https://api.spotify.com/v1/search?${params}`;
+    return this.http.request(queryURL);
+  }
+}
