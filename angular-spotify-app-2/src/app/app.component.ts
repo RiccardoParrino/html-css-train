0d660b23c4a975fcd2cf3b35966be1d877b64ab5
RiccardoParrino <riccardoparrino1@gmail.com>
2025-07-12 18:52:38 +0200

    content style

--- angular-spotify-app-2/src/app/app.component.ts
+++ angular-spotify-app-2/src/app/app.component.ts
@@ -1,21 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Track } from './track/track.model';
 import { SpotifyService } from './services/spotify.service';
 import { TrackComponent } from './track/track.component';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TrackComponent],
+  imports: [RouterOutlet, TrackComponent, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angular-spotify-app-2';
 
   results:Track[] = [];
 
   constructor (private spotifyService:SpotifyService){
+  }
+
+  ngOnInit(): void {
+    this.results = this.spotifyService.search("");
+    console.log(this.results);
   }
 
   search(query:string) {
