6b1e9e7c69e3ef74ed56bb72e8f2778f3295555b
RiccardoParrino <riccardoparrino1@gmail.com>
2025-07-12 15:08:00 +0200

    change styling of component

--- angular-spotify-app/src/app/content/content.component.ts
+++ angular-spotify-app/src/app/content/content.component.ts
@@ -1,17 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import { TrackComponent } from '../track/track.component';
 import { SpotifyService } from '../service/spotify.service';
 import { Track } from '../track/track.model';
 import { NgFor } from '@angular/common';
+import { TrackComponent } from '../track/track.component';
 
 @Component({
   selector: 'app-content',
-  imports: [NgFor],
+  imports: [NgFor, TrackComponent],
   templateUrl: './content.component.html',
   styleUrl: './content.component.css'
 })
 export class ContentComponent implements OnInit {
-  results:TrackComponent[] = [];
+  results:Track[] = [];
 
   constructor (private spotifyService: SpotifyService) {
   }
