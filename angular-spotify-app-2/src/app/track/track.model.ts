562a160a5633bcb2cba72db10976f0ff6bba92d6
RiccardoParrino <riccardoparrino1@gmail.com>
2025-07-12 18:22:50 +0200

    adding track module

---
+++ angular-spotify-app-2/src/app/track/track.model.ts
@@ -0,0 +1,9 @@
+export class Track {
+    constructor(
+        public imgUrl:string,
+        public name:string,
+        public artistName:string,
+        public album:string,
+        public date:string
+    ){}
+}
