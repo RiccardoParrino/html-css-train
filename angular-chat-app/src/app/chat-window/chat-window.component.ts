b9fbb08aa0e942a9a6be058376601aaecf880567
RiccardoParrino <riccardoparrino1@gmail.com>
2025-07-14 10:40:05 +0200

    adding some element to the chat window

--- angular-chat-app/src/app/chat-window/chat-window.component.ts
+++ angular-chat-app/src/app/chat-window/chat-window.component.ts
@@ -1,17 +1,20 @@
 import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'chat-window',
-  imports: [],
+  imports: [NgFor],
   templateUrl: './chat-window.component.html',
   styleUrl: './chat-window.component.css'
 })
 export class ChatWindowComponent {
 
   actualThread:string;
-
+  messages: string[];
+  
   constructor() {
     this.actualThread = "pippo";
+    this.messages = ["ciao come stai", "tutto bene", "anche io"];
   }
 
 }
