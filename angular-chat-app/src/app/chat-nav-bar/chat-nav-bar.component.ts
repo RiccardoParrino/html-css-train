997a50339f1292ea42729f00dde743b86ca7c647
RiccardoParrino <riccardoparrino1@gmail.com>
2025-07-14 09:34:59 +0200

    adding navbar in project

--- angular-chat-app/src/app/chat-nav-bar/chat-nav-bar.component.ts
+++ angular-chat-app/src/app/chat-nav-bar/chat-nav-bar.component.ts
@@ -8,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class ChatNavBarComponent {
 
+  unreadMessagesCount:Number;
+
+  constructor () {
+    this.unreadMessagesCount = 0;
+  }
+
 }
