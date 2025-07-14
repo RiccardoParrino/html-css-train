59d9c0db27ef93783f09ddc67bae5aea5bbb9c4a
RiccardoParrino <riccardoparrino1@gmail.com>
2025-07-14 08:56:59 +0200

    new angular components

---
+++ angular-chat-app/src/app/chat-nav-bar/chat-nav-bar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-nav-bar',
+  imports: [],
+  templateUrl: './chat-nav-bar.component.html',
+  styleUrl: './chat-nav-bar.component.css'
+})
+export class ChatNavBarComponent {
+
+}
---
+++ angular-chat-app/src/app/chat-threads/chat-threads.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-threads',
+  imports: [],
+  templateUrl: './chat-threads.component.html',
+  styleUrl: './chat-threads.component.css'
+})
+export class ChatThreadsComponent {
+
+}
