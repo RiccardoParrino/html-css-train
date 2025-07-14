5475d994af598a85584bbd4d0a2a67a7b5f3997e
RiccardoParrino <riccardoparrino1@gmail.com>
2025-07-14 09:22:23 +0200

    fixing some imports errors

--- angular-chat-app/src/app/chat-page/chat-page.component.ts
+++ angular-chat-app/src/app/chat-page/chat-page.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ChatWindowComponent } from '../chat-window/chat-window.component';
+import { ChatThreadsComponent } from '../chat-threads/chat-threads.component';
+import { ChatNavBarComponent } from '../chat-nav-bar/chat-nav-bar.component';
 
 @Component({
   selector: 'app-chat-page',
-  imports: [],
+  imports: [ChatWindowComponent, ChatThreadsComponent, ChatNavBarComponent],
   templateUrl: './chat-page.component.html',
   styleUrl: './chat-page.component.css'
 })
-export class ChatPageComponent {
+export class ChatPageComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit(): void {
+    
+  }
 
 }
