c8b2d72737924ef30decdfaa856603b05239acc9
RiccardoParrino <riccardoparrino1@gmail.com>
2025-07-15 10:33:08 +0200

    adding MessageServices

--- angular-chat-app/src/app/message/messages.service.ts
+++ angular-chat-app/src/app/message/messages.service.ts
@@ -1,12 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Observable, Subject } from 'rxjs';
+import { Subject } from 'rxjs';
 import { Message } from './message.model';
-
-const initialMessages: Message[] = [];
-
-interface IMessagesOperation extends Function {
-  (messages: Message[]): Message[];
-}
 
 @Injectable({
   providedIn: 'root'
@@ -15,17 +9,11 @@ export class MessagesService {
 
   newMessages: Subject<Message> = new Subject<Message>();
 
-  messages: Observable<Message[]>;
+  // messages: Observable<Message[]>;
 
   updates: Subject<any> = new Subject<any>();
 
   constructor() {
-    this.messages = this.updates
-      .scan( (messages: Message[],
-              operation: IMessageOperation) => {
-                return operation(messages);
-              }
-      ) )
   }
 
   addMessage(message: Message):void {
